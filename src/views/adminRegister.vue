bee23d6c7727839fa23900d045bf46cd1c65cdaf
Universamll <xpyluck@outlook.com>
2023-07-20 00:55:09 +0800

    已完成：登录；查看书籍详情；查看个人信息；退出登录；完善查询

---
+++ src/views/adminRegister.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "adminRegister"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/info.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <el-descriptions class="margin-top" title="书籍详情" :column="3" border>
+      <el-descriptions-item><template slot="label">书籍名称</template>{{ bookData.book_name }}
+      </el-descriptions-item>
+      <el-descriptions-item><template slot="label">作者</template>{{ bookData.author }}
+      </el-descriptions-item>
+      <el-descriptions-item><template slot="label">索书号</template>{{ bookData.book_id }}
+      </el-descriptions-item>
+      <el-descriptions-item><template slot="label">ISBN</template>{{ bookData.isbn }}
+      </el-descriptions-item>
+      <el-descriptions-item><template slot="label">价格</template>{{ bookData.price }}
+      </el-descriptions-item>
+    </el-descriptions>
+    <el-descriptions>
+      <el-descriptions-item><template slot="label">简介</template>{{ bookData.intro }}
+      </el-descriptions-item>
+    </el-descriptions>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "info",
+  data(){
+    return{
+      bookData:{
+        book_id: "",
+        book_name: "",
+        isbn: "",
+        author: "",
+        intro: "",
+        publisher: "",
+        price:""
+      },
+    }
+  },
+  created() {
+    let bookId=this.$route.query
+    this.$axios.get('/SearchBook/findOne?bookId='+bookId['book_id']).then(res=>{
+      this.bookData=res.data[0]
+    })
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
