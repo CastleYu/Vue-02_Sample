c3cf55a26bf81b6a302138f8c962a498ff8f0945
Yahk <747562686@qq.com>
2023-07-20 18:22:13 +0800

    跟进修改

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,72 +1,75 @@
 <template>
-  <el-container style="min-height: 100vh; border: 1px solid #eee">
+    <el-container style="min-height: 100vh; border: 1px solid #eee">
 
-    <el-aside :width="sideWidth+'px'" style="background-color: rgb(238, 241, 246);box-shadow: 2px 0 6px rgb(0 21 41/35%)">
-      <Aside :is-collapse="isCollapse" :logo-text-show="logoTextShow"/>
-    </el-aside>
+        <el-aside :width="sideWidth + 'px'"
+            style="background-color: rgb(238, 241, 246);box-shadow: 2px 0 6px rgb(0 21 41/35%); transition:width 0.5s;">
+            <Aside :is-collapse="isCollapse" :logo-text-show="logoTextShow" style="transition:width 0.5s;" />
+        </el-aside>
 
-    <el-container>
-      <el-header style="border-bottom: 1px solid #ccc;">
-        <Header :collapse-btn-class="collapseBtnClass" :collapse="collapse" :username="username['username']"/>
-      </el-header>
+        <el-container>
+            <el-header style="border-bottom: 1px solid #ccc;">
+                <Header :collapse-btn-class="collapseBtnClass" :collapse="collapse" :username="username['username']" />
+            </el-header>
 
-      <el-main>
-        <!-- 分页栏 -->
-        <el-breadcrumb separator-class="el-icon-arrow-right" style="padding: 5px 0">
-          <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-          <el-breadcrumb-item>书籍借阅</el-breadcrumb-item>
-        </el-breadcrumb>
+            <el-main>
+                <!-- 分页栏 -->
+                <el-breadcrumb separator-class="el-icon-arrow-right" style="padding: 5px 0">
+                    <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+                    <el-breadcrumb-item>书籍借阅</el-breadcrumb-item>
+                </el-breadcrumb>
 
-        <!-- 查询框 -->
-        <div style="padding: 10px 0">
-          <el-input
-              style="width: 200px;margin-right: 5px"
-              placeholder="条形码索书号"
-              prefix-icon="el-icon-document-copy"
-              v-model="bookID"></el-input>
-          <el-button type="primary" @click="findOne" style="margin-right: 25px">精确搜索</el-button>
+                <!-- 查询框 -->
+                <div style="padding: 10px 0">
+                    <el-input style="width: 200px;margin-right: 5px" placeholder="条形码索书号"
+                        prefix-icon="el-icon-document-copy" v-model="bookID"></el-input>
+                    <el-button type="primary" @click="findOne" style="margin-right: 25px">精确搜索</el-button>
 
-          <el-input
-              style="width: 450px;margin-right: 5px"
-              placeholder="支持 作品名 / 作者 / 出版商 / ISBN号 查询"
-              v-model="bookInfo"></el-input>
-          <el-button type="primary" @click="fuzzySearch">模糊搜索</el-button>
-          <el-button type="primary" @click="load">重置</el-button>
-        </div>
+                    <el-input style="width: 450px;margin-right: 5px" placeholder="支持 作品名 / 作者 / 出版商 / ISBN号 查询"
+                        v-model="bookInfo"></el-input>
+                    <el-button type="primary" @click="fuzzySearch">模糊搜索</el-button>
+                    <el-button type="primary" @click="load">重置</el-button>
+                </div>
 
-        <!-- 表格内容 -->
-        <el-table :data="tableData" border stripe>
-          <el-table-column prop="book_name" label="书籍名称" width="140">
-          </el-table-column>
-          <el-table-column prop="author" label="作者" width="120">
-          </el-table-column>
-          <el-table-column prop="publisher" label="出版商">
-          </el-table-column>
-          <el-table-column prop="isbn" label="ISBN号"></el-table-column>
-          <el-table-column prop="book_id" label="索书号"></el-table-column>
-          <el-table-column>
-            <template slot-scope="scope">
-              <el-button type="primary" @click="toDetail(scope.row)">详情</el-button>
-              <el-button type="success" @click="borrow">借阅 <i class="el-icon-reading"></i></el-button>
-            </template>
-          </el-table-column>
-        </el-table>
+                <!-- 表格内容 -->
+                <el-table :data="tableData" border stripe>
+                    <el-table-column prop="book_name" label="书籍名称" width="140" />
+                    <el-table-column prop="author" label="作者" width="120" />
+                    <el-table-column prop="publisher" label="出版商" />
+                    <el-table-column prop="isbn" label="ISBN号" />
+                    <el-table-column prop="book_id" label="索书号" />
+                    <el-table-column>
+                        <template slot-scope="scope">
+                            <el-button type="primary" @click="showDetail(scope.row);">详情</el-button>
+                            <el-button type="success" @click="borrow"><i class="el-icon-reading" /> 借阅</el-button>
+                        </template>
+                    </el-table-column>
+                </el-table>
 
-        <!-- 表格底部分页栏 -->
-        <div style="padding: 10px 0">
-          <el-pagination
-              @size-change="handleSizeChange"
-              @current-change="handleCurrentChange"
-              :current-page="pageNum"
-              :page-sizes="[2, 5, 10, 20]"
-              :page-size="pageSize"
-              layout="total, sizes, prev, pager, next, jumper"
-              :total="total">
-          </el-pagination>
-        </div>
-      </el-main>
+                <!-- 表格底部分页栏 -->
+                <div style="padding: 10px 0">
+                    <el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange"
+                        :current-page="pageNum" :page-sizes="[2, 5, 10, 20]" :page-size="pageSize"
+                        layout="total, sizes, prev, pager, next, jumper" :total="total">
+                    </el-pagination>
+                </div>
+
+                <el-dialog title="书籍详情" :visible.sync="infoDialog" @close="clearDetail" width="27.5vw">
+                    <el-descriptions class="margin-top" title="书籍详情" :column="1" border >
+                        <el-descriptions-item label="书籍名称">{{ bookData.book_name }}</el-descriptions-item>
+                        <el-descriptions-item label="作者">{{ bookData.author }}</el-descriptions-item>
+                        <el-descriptions-item label="索书号">{{ bookData.book_id }}</el-descriptions-item>
+                        <el-descriptions-item label="ISBN">{{ bookData.isbn }}</el-descriptions-item>
+                        <el-descriptions-item label="价格">{{ bookData.price }}</el-descriptions-item>
+                    </el-descriptions>
+                    <el-descriptions style="margin: 15px;">
+                        <el-descriptions-item label="简介">{{ bookData.intro }}
+                        </el-descriptions-item>
+                    </el-descriptions>
+                </el-dialog>
+
+            </el-main>
+        </el-container>
     </el-container>
-  </el-container>
 </template>
 
 <script>
@@ -76,11 +79,11 @@ import Header from "@/components/Header";
 export default {
     name: 'HomeView',
     components: {
-      Aside,Header
+        Aside, Header
     },
     data() {
         return {
-            username:"",
+            username: "",
 
             tableData: [],
             collapseBtnClass: 'el-icon-s-fold',
@@ -89,77 +92,101 @@ export default {
             logoTextShow: true,
             show3: true,
             showModal: false,
-            iconClass:"el-icon-arrow-down",
+            iconClass: "el-icon-arrow-down",
 
-            pageNum:1,
-            pageSize:5,
-            total:0,
+            pageNum: 1,
+            pageSize: 5,
+            total: 0,
 
-            bookID:'',
-            bookInfo:''
+            bookID: '',
+            bookInfo: '',
+
+            bookData: {
+                book_id: "",
+                book_name: "",
+                isbn: "",
+                author: "",
+                intro: "",
+                publisher: "",
+                price: ""
+            },
+            infoDialog: false,
         }
     },
     created() {
-      this.username=this.$route.query
-      this.load()
+        this.username = this.$route.query
+        this.load()
     },
-    methods:{
-      collapse(){//点击收缩时触发
-        this.isCollapse = !this.isCollapse;
-        if (this.isCollapse){//收缩状态
-          this.sideWidth=64;
-          this.collapseBtnClass='el-icon-s-unfold'
-          this.logoTextShow=false
-        }else{//展开
-          this.sideWidth=200
-          this.collapseBtnClass='el-icon-s-fold'
-          this.logoTextShow=true
+    methods: {
+        collapse() {//点击收缩时触发
+            this.isCollapse = !this.isCollapse;
+            if (this.isCollapse) {//收缩状态
+                this.sideWidth = 64;
+                this.collapseBtnClass = 'el-icon-s-unfold'
+                this.logoTextShow = false
+            } else {//展开
+                this.sideWidth = 200
+                this.collapseBtnClass = 'el-icon-s-fold'
+                this.logoTextShow = true
+            }
+        },
+        load() {//进入界面时触发,加载所有图书信息
+            this.$axios.get('/SearchBook/findAll').then(res => {
+                this.tableData = res.data
+                this.total = res.data.length
+
+                this.bookInfo = ""
+                this.bookID = ""
+                console.log("载入log")
+            })
+        },
+        findOne() {//根据条形码索书号完全匹配查询
+            this.$axios.get('/SearchBook/findOne?bookId=' + this.bookID).then(res => {
+                this.tableData = res.data
+                this.total = res.data.length
+                console.log("精确查询log")
+            })
+        },
+        fuzzySearch() {//根据图书相关信息模糊查询
+            this.$axios.get('/SearchBook/fuzzySearch?pageNum='
+                + this.pageNum + '&pageSize=' + this.pageSize + '&searchCond=' + this.bookInfo).then(res => {
+                    this.tableData = res.data
+                    console.log("模糊查询log")
+                })
+        },
+        handleSizeChange(pageSize) {
+            this.pageSize = pageSize
+            this.fuzzySearch()
+        },
+        handleCurrentChange(pageNum) {
+            this.pageNum = pageNum
+            this.fuzzySearch()
+        },
+        borrow() {//点击借阅时触发
+
+            console.log("借阅log")
+        },
+        showDetail(row) {
+            if (!row.book_id)console.log(row.book_id)
+            this.$axios.get('/SearchBook/findOne?bookId=' + row.book_id).then(res => {
+                this.bookData = res.data[0]
+            })
+            console.log("详情log")
+            this.infoDialog = true;
+        },
+        clearDetail(){
+            this.bookData={
+                book_id: "",
+                book_name: "",
+                isbn: "",
+                author: "",
+                intro: "",
+                publisher: "",
+                price: ""
+            }
         }
-      },
-      load(){//进入界面时触发,加载所有图书信息
-        this.$axios.get('/SearchBook/findAll').then(res => {
-          this.tableData=res.data
-          this.total=res.data.length
-
-          this.bookInfo=""
-          this.bookID=""
-          console.log("载入log")
-        })
-      },
-      findOne(){//根据条形码索书号完全匹配查询
-        this.$axios.get('/SearchBook/findOne?bookId='+this.bookID).then(res => {
-          this.tableData=res.data
-          this.total=res.data.length
-          console.log("精确查询log")
-        })
-      },
-      fuzzySearch(){//根据图书相关信息模糊查询
-        this.$axios.get('/SearchBook/fuzzySearch?pageNum='
-            +this.pageNum+'&pageSize='+this.pageSize+'&searchCond='+this.bookInfo).then(res => {
-          this.tableData=res.data
-          console.log("模糊查询log")
-        })
-      },
-      handleSizeChange(pageSize) {
-        this.pageSize=pageSize
-        this.fuzzySearch()
-      },
-      handleCurrentChange(pageNum) {
-        this.pageNum=pageNum
-        this.fuzzySearch()
-      },
-      borrow(){//点击借阅时触发
-
-        console.log("借阅log")
-      },
-      toDetail(row){
-        this.$router.push({path:'/info',query:{book_id:row.book_id}})
-        console.log("详情log")
-      }
     }
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
--- src/views/account/LoginView.vue
+++ src/views/account/LoginView.vue
@@ -1,24 +1,27 @@
 <template>
     <div class="back">
-        <div
-            style="margin: 200px auto; background-color: #fff; width: 350px; height: 280px; padding: 20px; border-radius: 10px">
+        <div class="floatCard"
+                style="margin: 200px auto; background-color: #fff; width: 360px; height: 280px; padding: 20px; border-radius: 10px">
             <div style="margin: 20px 0; text-align: center; font-size: 20px"><b>图 书 借 阅 系 统</b></div>
             <el-form :model="form" ref="userForm">
+
                 <el-form-item prop="username">
                     <el-input size="medium" prefix-icon="el-icon-user" v-model="form.username" placeholder="用户名"></el-input>
                 </el-form-item>
+
                 <el-form-item prop="password">
                     <el-input size="medium" prefix-icon="el-icon-lock" show-password v-model="form.password" @keyup.enter.native="handleSubmit"
                         placeholder="密码"></el-input>
                 </el-form-item>
-                <el-form-item style="margin: 10px 0; text-align: left">
-                    <el-button style="margin-right: 128px;border: transparent;color:chocolate"
-                        @click="$router.push('/adminLogin')">管理员登录</el-button>
-                    <el-button style="border: transparent" size="small" autocomplete="off"
-                        @click="$router.push('/userRegister')">注册</el-button>
-                    <el-button type="primary" size="small" autocomplete="off" @click="handleSubmit">登录</el-button>
 
+                <el-form-item style="margin: 10px 0;">
+                    <el-button style="border: transparent;color:chocolate;float: left;"
+                        @click="$router.push('/adminLogin')">管理员登录</el-button>
+                        <el-button style="border: transparent;float: right" type="primary" size="small" autocomplete="off" @click="handleSubmit">登录</el-button>
+                    <el-button style="border: transparent;float: right" size="small" autocomplete="off" 
+                        @click="$router.push('/userRegister')">注册</el-button>
                 </el-form-item>
+
             </el-form>
         </div>
     </div>
@@ -45,15 +48,16 @@ export default {
     },
     methods: {
         handleSubmit() {// 在这里添加登录逻辑
+            this.$message("正在登录")
             this.$axios.get('/userLoginToSystem?username='
                 + this.form.username + '&password=' + this.form.password).then(res => res.data).then(res => {
                     if (res.data === '登录成功') {
                         console.log('用户名:', this.form.username);
                         console.log('密码:', this.form.password);
-                        alert("登录成功")
+                        this.$message.success("登录成功")
                         router.push({ path: '/home', query: { username: this.form.username } })
                     } else {
-                        alert("登录失败，请检查用户名或密码是否正确")
+                        this.$message.error("登录失败，请检查用户名或密码是否正确")
                     }
                 })
         }
@@ -71,6 +75,11 @@ export default {
     background: -webkit-linear-gradient(to bottom right, #FF8235, #30E8BF);
     background: linear-gradient(to bottom right, #FF8235, #30E8BF);
     margin: 0;
+}
+.floatCard {
+    box-shadow: 0px 4px 50px rgba(0, 0, 0, 0.25);
+    /* 水平阴影的位置(+向右) | 垂直阴影的位置(+向下) | 模糊距离 | 阴影色彩 */
+    border-radius: 10px
 }
 </style>
   
