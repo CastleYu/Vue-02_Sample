b3eefc6a2c288acbe19909d301db3dc168f68945
Yahk <747562686@qq.com>
2023-07-20 20:33:44 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from './router'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
-import './outlook/mainfont.css'
+import '@/outlook/mainfont.css'
 import axios from "axios";
 
 Vue.use(ElementUI,{size:"mini"});
